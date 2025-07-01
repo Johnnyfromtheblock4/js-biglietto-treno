8ec4e36bb7e70070338b6332bb5b947db8725a5a
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-01 17:33:57 +0200

    [UPD] risultato-finale

--- js/script.js
+++ js/script.js
@@ -8,11 +8,12 @@ let totalPrice = km * priceKm;
 console.log(totalPrice);
 
 //calcolo biglietto sconto
-if (age <= 18) {
+if (age < 18) {
   const discount = (totalPrice * 20) / 100;
-  totalPrice - discount;
-} else if (age >= 18) {
+  totalPrice = totalPrice - discount;
+} else if (age > 65) {
   const discount = (totalPrice * 40) / 100;
-  totalPrice - discount;
+  totalPrice = totalPrice - discount;
 }
 
+console.log(totalPrice.toFixed(2));
