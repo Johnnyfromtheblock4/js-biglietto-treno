3816a2680abe52ff5e74d3f74d061f1f5dff82ae
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-01 17:05:13 +0200

    [UPD] prezzo-intero

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 const km = parseFloat(prompt(`inserisci i km`));
 const age = parseInt(prompt(`inserisci l'età`));
 const priceKm = 0.21;
+
+//calcolo prezzo senza sconti
+let totalPrice = km * priceKm;
+console.log(totalPrice)
+
+//biglietto sconto
+if (age >= 18) {
+  const discount = (totalPrice * 20) /100;
+  
+}
