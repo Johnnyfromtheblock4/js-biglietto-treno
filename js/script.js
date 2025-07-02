93e99a0cd5698718019b81d9c2729a94c7cc9d1a
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-02 10:20:22 +0200

    [UPD] correzione-esercizio

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 //definizione variabili
 const km = parseFloat(prompt(`inserisci i km`));
 const age = parseInt(prompt(`inserisci l'età`));
-const priceKm = 0.21;
+
+//variabile che conterrà il prezzo finale da mostrare
+let finalPrice;
+let message;
 
 //calcolo prezzo biglietto senza sconti
-totalPrice = km * priceKm;
+const basePrice = km * 0.21;
 
-//calcolo biglietto sconto
+//calcolo biglietto scontato
 if (age < 18) {
-  const discount = (totalPrice * 20) / 100;
-  totalPrice = totalPrice - discount;
+  finalPrice = basePrice - (basePrice * 20) / 100;
+  message = `Hai diritto ad uno sconto del 20% in quanto minorenne. Il prezzo del biglietto è pari a ${finalPrice}`;
 } else if (age > 65) {
-  const discount = (totalPrice * 40) / 100;
-  totalPrice = totalPrice - discount;
+  message = `Hai diritto ad uno sconto del 20% in quanto over 65. Il prezzo del biglietto è pari a ${finalPrice}`;
+  finalPrice = basePrice - (basePrice * 40) / 100;
+} else {
+  finalPrice = basePrice;
+  message = `Il prezzo del biglietto è pari a ${finalPrice}`;
 }
 
-console.log(totalPrice.toFixed(2));
+console.log(`Il prezzo finale del biglietto è di ${finalPrice.toFixed(2)}`);
